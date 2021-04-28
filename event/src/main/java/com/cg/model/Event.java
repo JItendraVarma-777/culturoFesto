2f95be7ad1c11ce61cc6a678b9ea143ed9f304a1
abc <abc@example.com>
2021-04-28 12:27:42 +0530

    first commit

---
+++ event/src/main/java/com/cg/model/Event.java
@@ -0,0 +1,167 @@
+package com.cg.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.sql.Time;
+
+import org.springframework.stereotype.Component;
+@Component
+@Entity
+@Table(name = "Event")
+public class Event {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long eventId;
+
+	private String name;
+
+	private LocalDate eventDate;
+
+	private float amount;
+	
+	private float foodfee;
+	
+	private LocalDate finalRegDate;
+
+	private Time startTime;
+
+	private Time endTime;
+
+	private String venue;
+	
+	private long  limits;
+    
+
+	public Event() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Event(long eventId, String name, LocalDate eventDate, float amount, float foodfee, LocalDate finalRegDate,
+			Time startTime, Time endTime, String venue, long limits) {
+		super();
+		this.eventId = eventId;
+		this.name = name;
+		this.eventDate = eventDate;
+		this.amount = amount;
+		this.foodfee = foodfee;
+		this.finalRegDate = finalRegDate;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.venue = venue;
+		this.limits = limits;
+	}
+
+
+	public long getEventId() {
+		return eventId;
+	}
+
+
+	public void setEventId(long eventId) {
+		this.eventId = eventId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public LocalDate getEventDate() {
+		return eventDate;
+	}
+
+
+	public void setEventDate(LocalDate eventDate) {
+		this.eventDate = eventDate;
+	}
+
+
+	public float getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+
+	public float getFoodfee() {
+		return foodfee;
+	}
+
+
+	public void setFoodfee(float foodfee) {
+		this.foodfee = foodfee;
+	}
+
+
+	public LocalDate getFinalRegDate() {
+		return finalRegDate;
+	}
+
+
+	public void setFinalRegDate(LocalDate finalRegDate) {
+		this.finalRegDate = finalRegDate;
+	}
+
+
+	public Time getStartTime() {
+		return startTime;
+	}
+
+
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+
+
+	public Time getEndTime() {
+		return endTime;
+	}
+
+
+	public void setEndTime(Time endTime) {
+		this.endTime = endTime;
+	}
+
+
+	public String getVenue() {
+		return venue;
+	}
+
+
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+
+
+	public long getLimits() {
+		return limits;
+	}
+
+
+	public void setLimits(long limits) {
+		this.limits = limits;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Event [eventId=" + eventId + ", name=" + name + ", eventDate=" + eventDate + ", amount=" + amount
+				+ ", foodfee=" + foodfee + ", finalRegDate=" + finalRegDate + ", startTime=" + startTime + ", endTime="
+				+ endTime + ", venue=" + venue + ", limits=" + limits + "]";
+	}
+
+
+
+}
